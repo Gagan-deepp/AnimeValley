4484b3aef1f1dbfcaada5b655c54da7db623e6f5
Gagandeep SIngh <gagan270903@gmail.com>
2024-04-08 10:28:41 +0530

    Navitems edit

--- components/webcomp/NavItems.jsx
+++ components/webcomp/NavItems.jsx
@@ -7,11 +7,18 @@ import { usePathname } from 'next/navigation'
 const NavItems = () => {
 
     const pathname = usePathname();
+    let isActive
     return (
         <div>
             <ul className='flex gap-3 justify-evenly'>
                 {navitemData.map((items, index) => {
-                    const isActive = pathname.includes(items.href)
+                    if (pathname !== "/" && pathname.includes(items.href)) {
+                        isActive = true
+                    }
+                    else if (pathname === "/" && items.href === "/") {
+                        isActive = true
+                    }
+                    else isActive = false
                     return (
                         <li key={index} className={` text-gray-3 relative flex-center whitespace-nowrap `} >
                             {isActive && <motion.span layoutId='navBgHome' className='bg-light-2 absolute top-0 left-0 right-0 w-full h-full rounded-2xl z-[-1]' />}
